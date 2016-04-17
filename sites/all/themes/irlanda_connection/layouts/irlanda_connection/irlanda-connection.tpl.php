487ef0ea500dbbb8ba862f4cc54c6fe6ee4d47a8
Javier Torrado <javiertorrado@galicia.com>
2016-04-17 22:15:10 +0100

    Services Page added

--- sites/all/themes/irlanda_connection/layouts/irlanda_connection/irlanda-connection.tpl.php
+++ sites/all/themes/irlanda_connection/layouts/irlanda_connection/irlanda-connection.tpl.php
@@ -28,14 +28,12 @@
   </div>
   <div class="section-main">
     <div class="section-content" role="main">
-      <?php print render($page['highlighted']); ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
         <h1><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
       <?php print render($tabs); ?>
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
