074660f77659c9c93ccfcd1972bec102dc3b75fa
Javier Torrado <javiertorrado@galicia.com>
2016-05-08 20:08:42 +0100

    Course and Blog Post Page Done

--- sites/all/themes/irlanda_connection/layouts/irlanda_connection/irlanda-connection.tpl.php
+++ sites/all/themes/irlanda_connection/layouts/irlanda_connection/irlanda-connection.tpl.php
@@ -30,9 +30,6 @@
     <div class="section-content" role="main">
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
-      <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print render($tabs); ?>
       <?php if ($action_links): ?>
